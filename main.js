9d21a64476f257a8f43c9e5e94fc07864cb425e9
itsace003 <44100742+itsace003@users.noreply.github.com>
2025-07-29 14:44:28 +1000

    Center hero image on large screens

--- main.js
+++ main.js
@@ -35,9 +35,10 @@ const sr = ScrollReveal({
 })
 
 sr.reveal('.home-title', {} )
+sr.reveal('.home-subtitle', {delay: 100} )
 sr.reveal('.button', {delay: 200} )
 sr.reveal('.home-img', {delay: 400} )
-sr.reveal('.home-social', {delay: 400,} )
+sr.reveal('.home-social', {delay: 400} )
 
 sr.reveal('.about-img', {} )
 sr.reveal('.about-subtitle', {delay: 200} )
